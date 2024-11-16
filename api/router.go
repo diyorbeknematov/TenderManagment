e557708e6f9aaf499fe3e39ce58eb417955622df
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-11-17 01:46:35 +0500

    all

--- api/router.go
+++ api/router.go
@@ -6,6 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @title 						TENDER MANAGMENT API
+// @version 					0.1
+// @description 				This is a sample API.
+// @schemes 					http https
+// @BasePath 					/
+// @consumes 					application/json
+// @produces 					application/json
+// @securityDefinitions.apiKey 	Bearer
+// @in 							header
+// @name 						Authorization
+// @swagger:meta
 func Router(h *handler.Handler) *gin.Engine {
 	router := gin.Default()
 

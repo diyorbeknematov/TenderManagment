7be25a1b1503cc0c0afe8ff4c52b743b247a9b53
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-11-16 21:54:08 +0500

    Update

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -1,1 +1,18 @@
 package handler
+
+import (
+	"log/slog"
+	"tender/service"
+)
+
+type Handler struct{
+	Service service.Service
+	Log *slog.Logger
+}
+
+func NewHandler(service service.Service, logger *slog.Logger)*Handler{
+	return &Handler{
+		Service: service,
+		Log: logger,
+	}
+}

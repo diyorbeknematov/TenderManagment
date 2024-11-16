a128b7284b74f6de3210bcda4f50de600903810c
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-11-16 16:20:13 +0500

    Add db connect

--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,22 @@
 package storage
+
+import (
+	"database/sql"
+	"log/slog"
+)
+
+type Storage interface{
+
+}
+
+type storageImpl struct{
+	DB *sql.DB
+	Log *slog.Logger
+}
+
+func NewStorage(db *sql.DB, logger *slog.Logger)Storage{
+	return &storageImpl{
+		DB: db,
+		Log: logger,
+	}
+}
